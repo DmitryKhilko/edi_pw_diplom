6bbbdc1d224842e5e3d6d340b111ca1604fedabf
DmitryKhilko <dmitrykhilko@mail.ru>
2023-06-19 00:10:59 +0300

    framework modification: refactor test_ui_login.py

--- tests/test_ui_login.py
+++ tests/test_ui_login.py
@@ -17,7 +17,7 @@ class TestUILogin:
         'Проверка возможности входа в приложение c использованием графического интерфейса '
         'под всеми ролями приложения с валидными значениями логина и пароля'
     )
-    # @allure.id("1371")
+    @allure.id("1375")
     @allure.title('Вход в приложение (валидные значения логина и пароля)')
     def test_ui_can_login_by_role(self, web_app, user, parameter_description, expected_result):
         logging.debug(f'Начать тест "Вход в приложение (валидные значения логина и пароля)" для роли "{user[0]}"')
@@ -26,22 +26,3 @@ class TestUILogin:
         login_page.login_by_role(user, parameter_description)
         login_page.check_login(expected_result)
         logging.debug(f'Окончить тест "Вход в приложение (валидные значения логина и пароля)" для роли "{user[0]}"')
-
-
-
-
-
-
-    # @mark.parametrize('p01_login, p02_password, result01_fio, result02_user_role, result03_user_organization',
-    #                   login_parameters)
-    # @allure.title(f'Вход пользователя в EDI-Flow по валидным логину и паролю')
-    # def test_ui_can_login_by_role(self, web_app, p01_login, p02_password, result01_fio, result02_user_role,
-    #                        result03_user_organization):
-    #     login_page = Login(web_app)
-    #     login_page.navigate()
-    #     login_page.login(p01_login, p02_password)
-    #     login_page.check_login(result01_fio, result02_user_role, result03_user_organization)
-    #
-    #
-    # def test_ui_can_not_login(self):
-    #     pass
